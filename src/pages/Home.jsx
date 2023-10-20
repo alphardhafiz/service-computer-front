a11d34e0c3db6a734afacce04916a473c76d4056
unknown <togarandreasmanik@yahoo.com>
2023-10-20 15:34:21 +0700

    memperbaiki logo di home.jsx yang dibawah

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,21 +43,21 @@ const Home = () => {
                 <div class="row">
                     <div class="col-lg-4">
                         <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                        <img class="img-fluid rounded-circle mb-3" src="src/assets/logo.png" alt="..." />
+                        <img class="img-fluid " src="src/assets/logo.png" alt="..." />
                             <h3>Fully Responsive</h3>
                             <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                        <img class="img-fluid rounded-circle mb-3" src="src/assets/logo.png" alt="..." />
+                        <img class="img-fluid " src="src/assets/logo.png" alt="..." />
                             <h3>Bootstrap 5 Ready</h3>
                             <p class="lead mb-0">Featuring the latest build of the new Bootstrap 5 framework!</p>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-                        <img class="img-fluid rounded-circle mb-3" src="src/assets/logo.png" alt="..." />
+                        <img class="img-fluid " src="src/assets/logo.png" alt="..." />
                             <h3>Easy to Use</h3>
                             <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
                         </div>
