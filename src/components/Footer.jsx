9303dea2dc9bbc7d1cc96206701d7531a988998f
kazumo92 <adityayusuf92@gmail.com>
2023-10-20 10:01:12 +0700

    ubah posisi footer jd kebawah

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div style={{bottom:'0', backgroundColor:'black', color:'white', width:'100%', position:'absolute'}}>
+    <div style={{top:'0 rem', backgroundColor:'black', color:'white', width:'100%', position:'absolute'}}>
 
     <h2 style={{textAlign:'center'}}>copyright by kelompok 1</h2>
 
