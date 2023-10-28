5c995f153de7d8fbeb26e733686a539b54b49119
alphardhafiz <bucialphard@gmail.com>
2023-10-28 08:49:05 +0700

    add feature send email

--- src/pages/PageContact.jsx
+++ src/pages/PageContact.jsx
@@ -1,87 +1,177 @@
-import React from 'react'
-import './PageContact.css'
+import React, { useState } from "react";
+import "./PageContact.css";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const PageContact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3000/api/barang/send-email", {
+        name: name,
+        email: email,
+        subject: subject,
+        phone: phone,
+        message: message,
+      });
+      Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "Email Berhasil Dikirim",
+      });
+    } catch (error) {
+      console.error("Error sending data", error.response.data.message);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: `Email Gagal Dikirim. ${error.response.data.message}`,
+      });
+    }
+  };
+
   return (
     <div className="contact3 py-5">
-  <div className="row no-gutters">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-6">
-          <div className="card-shadow">
-            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/2.jpg" className="img-fluid"/>
-          </div>
-        </div>
-        <div className="col-lg-6">
-          <div className="contact-box ml-3">
-            <h1 className="font-weight-light mt-2">Quick Contact</h1>
-            <form className="mt-4">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="form-group mt-2">
-                    <input className="form-control" type="text" placeholder="name"/>
-                  </div>
-                </div>
-                <div className="col-lg-12">
-                  <div className="form-group mt-2">
-                    <input className="form-control" type="email" placeholder="email address"/>
-                  </div>
-                </div>
-                <div className="col-lg-12">
-                  <div className="form-group mt-2">
-                    <input className="form-control" type="text" placeholder="phone"/>
-                  </div>
-                </div>
-                <div className="col-lg-12">
-                  <div className="form-group mt-2">
-                    <textarea className="form-control" rows="3" placeholder="message"></textarea>
-                  </div>
-                </div>
-                <div className="col-lg-12">
-                  <button type="submit" className="btn btn-danger-gradiant mt-3 text-white border-0 px-3 py-2"><span> SUBMIT</span></button>
-                </div>
+      <div className="row no-gutters">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="card-shadow">
+                <img
+                  src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/2.jpg"
+                  className="img-fluid"
+                />
               </div>
-            </form>
-          </div>
-        </div>
-        <div className="col-lg-13">
-          <div className="card mt-4 border-0 mb-4" >
-            <div className="row" >
-              <div className="col-lg-4 col-md-4">
-                <div className="card-body d-flex align-items-center c-detail pl-0" >
-                  <div className="mr-3 align-self-center">
-                    <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon1.png"/>
+            </div>
+            <div className="col-lg-6">
+              <div className="contact-box ml-3">
+                <h1 className="font-weight-light mt-2">Quick Contact</h1>
+                <form onSubmit={sendEmail} className="mt-4">
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <div className="form-group mt-2">
+                        <input
+                          className="form-control"
+                          type="text"
+                          placeholder="name"
+                          name="name"
+                          value={name}
+                          onChange={(e) => {
+                            console.log(e.target.value);
+                            setName(e.target.value);
+                          }}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-lg-12">
+                      <div className="form-group mt-2">
+                        <input
+                          className="form-control"
+                          type="email"
+                          name="email"
+                          placeholder="email address"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-lg-12">
+                      <div className="form-group mt-2">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="subject"
+                          placeholder="subject"
+                          value={subject}
+                          onChange={(e) => setSubject(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-lg-12">
+                      <div className="form-group mt-2">
+                        <input
+                          className="form-control"
+                          type="text"
+                          placeholder="phone"
+                          name="phone"
+                          value={phone}
+                          onChange={(e) => setPhone(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-lg-12">
+                      <div className="form-group mt-2">
+                        <textarea
+                          className="form-control"
+                          rows="3"
+                          placeholder="message"
+                          name="message"
+                          value={message}
+                          onChange={(e) => setMessage(e.target.value)}
+                        ></textarea>
+                      </div>
+                    </div>
+                    <div className="col-lg-12">
+                      <button
+                        type="submit"
+                        className="btn btn-danger-gradiant mt-3 text-white border-0 px-3 py-2"
+                      >
+                        <span>Send Email</span>
+                      </button>
+                    </div>
                   </div>
-                  <div className="">
-                    <h6 className="font-weight-medium">Address</h6>
-                    <p className="">601 Sherwood Ave.
-                      <br/> San Bernandino</p>
-                  </div>
-                </div>
+                </form>
               </div>
-              <div className="col-lg-4 col-md-4">
-                <div className="card-body d-flex align-items-center c-detail">
-                  <div className="mr-3 align-self-center">
-                    <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon2.png"/>
+            </div>
+            <div className="col-lg-13">
+              <div className="card mt-4 border-0 mb-4">
+                <div className="row">
+                  <div className="col-lg-4 col-md-4">
+                    <div className="card-body d-flex align-items-center c-detail pl-0">
+                      <div className="mr-3 align-self-center">
+                        <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon1.png" />
+                      </div>
+                      <div className="">
+                        <h6 className="font-weight-medium">Address</h6>
+                        <p className="">
+                          601 Sherwood Ave.
+                          <br /> San Bernandino
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="">
-                    <h6 className="font-weight-medium">Phone</h6>
-                    <p className="">251 546 9442
-                      <br/> 630 446 8851</p>
+                  <div className="col-lg-4 col-md-4">
+                    <div className="card-body d-flex align-items-center c-detail">
+                      <div className="mr-3 align-self-center">
+                        <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon2.png" />
+                      </div>
+                      <div className="">
+                        <h6 className="font-weight-medium">Phone</h6>
+                        <p className="">
+                          251 546 9442
+                          <br /> 630 446 8851
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-4">
-                <div className="card-body d-flex align-items-center c-detail">
-                  <div className="mr-3 align-self-center">
-                    <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon3.png"/>
-                  </div>
-                  <div className="">
-                    <h6 className="font-weight-medium">Email</h6>
-                    <p className="">
-                      info@wrappixel.com
-                      <br/> 123@wrappixel.com
-                    </p>
+                  <div className="col-lg-4 col-md-4">
+                    <div className="card-body d-flex align-items-center c-detail">
+                      <div className="mr-3 align-self-center">
+                        <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon3.png" />
+                      </div>
+                      <div className="">
+                        <h6 className="font-weight-medium">Email</h6>
+                        <p className="">
+                          info@wrappixel.com
+                          <br /> 123@wrappixel.com
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -90,9 +180,7 @@ const PageContact = () => {
         </div>
       </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default PageContact
+export default PageContact;
